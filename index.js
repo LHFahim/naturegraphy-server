fd7b030fa1f27f01c0f01df4da43c9d4ff003b29
Fahim-H <fahimlutful@gmail.com>
2022-11-10 21:37:45 +0600

    api for individual service

--- index.js
+++ index.js
@@ -43,6 +43,13 @@ async function run() {
       const result = await serviceCollection.insertOne(service);
       res.send(result);
     });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
   } finally {
   }
 }
